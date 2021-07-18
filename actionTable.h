8c0c6bbfb68ed97cc6f368a055f56acd6ae28486
adishik <60340634+adishik@users.noreply.github.com>
2021-07-18 15:21:36 +0300

    Update actionTable.h
    After changes that make action table work

--- actionTable.h
+++ actionTable.h
@@ -6,9 +6,20 @@
 #ifndef __ACTION_TABLE__
 #define __ACTION_TABLE__
 
-struct _Action;
+struct _Action // define action in Assembly 
+{
+    char * actionName;
+    char actionType;
+    int funct;
+    int opcode;
+    int numOfop;
+};
 typedef struct _Action Action;
 
-int * doRType(char * actionName,int * rs, int * rt, int * rd);
+int * convDectoBin(int decimalNumber, int  * binaryNumber); // converts a Decimal number to binary
+Action  * getAction(char * actionName); // get string and return Avrion object
+
+int * codeAction(Action  * action, int rs, int rt, int rd ); // truining action into binary
 
 #endif
+
