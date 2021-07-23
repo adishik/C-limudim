b12f1e534ec4fe246629610402bdb236cf4001b6
adishik <60340634+adishik@users.noreply.github.com>
2021-07-23 18:38:40 +0300

    Create instructions.h

---
+++ instructions.h
@@ -0,0 +1,9 @@
+#include "instructions.h"
+#define byte 1
+
+#ifndef __INSTRUCTION_TABLE__
+#define __INSTRUCTION_TABLE__
+
+int  ** freeDInstructions(int instructionType, char instructionSubType,int * intstructionParms, int parmsSize);
+
+#endif
