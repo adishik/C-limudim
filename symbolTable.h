ed252ea6259c29a804bca07df105b6e2add77fcc
adishik <60340634+adishik@users.noreply.github.com>
2021-07-24 17:52:22 +0300

    Create symbolTable.h

---
+++ symbolTable.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct _SymbolNode // define action in Assembly 
+{
+    char * symbole;
+    int val;
+    char * att;
+    struct _SymbolNode nextNode;
+};
+typedef struct _SymbolNode SymbolNode;
+
+struct SymbolNode * createNode();
+void addSymbole(char * symbolName, int val, char * att);
