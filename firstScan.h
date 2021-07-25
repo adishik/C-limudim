69066ee6ff86c7b61020af626b183be23159364b
adishik <60340634+adishik@users.noreply.github.com>
2021-07-25 18:53:57 +0300

    Create firstScan.h

---
+++ firstScan.h
@@ -0,0 +1,22 @@
+#include "parser.h"
+#include "symbolTable.h"
+#include "actionTable.h"
+#include "instructions.h"
+#include "intNode.h"
+
+#ifndef __FIRST_SCAN__
+#define __FIRST_SCAN__
+
+struct _FirstScan // define action in Assembly 
+{
+    SymbolNode * firstSymbol;
+    IntNode * firstLine;
+    IntNode * currentLine;
+    int IC;
+    int DC;
+};
+typedef struct _FirstScan FirstScan;
+bool checkEmpty(char * line);
+FirstScan * doScan(char * asFile);
+
+#endif
