abdd1f96e72bb026a94a692eb93b23bd8cf877bf
adishik <60340634+adishik@users.noreply.github.com>
2021-08-13 03:05:50 +0300

    Update firstScan.h

--- firstScan.h
+++ firstScan.h
@@ -7,9 +7,12 @@
 #ifndef __FIRST_SCAN__
 #define __FIRST_SCAN__
 
-struct _FirstScan
+/* this moudle will be used in order to make the first scan*/
+
+struct _FirstScan /*this what will be returned and given to the second scan*/
 {
     SymbolNode * firstSymbol;
+    SymbolNode * currentSymbol;
     IntNode * firstLine;
     IntNode * currentLine;
     int IC;
@@ -17,9 +20,10 @@ struct _FirstScan
     int lineCounter;
 };
 typedef struct _FirstScan FirstScan;
-int checkEmpty(char * line);
-int isAlphabet(char firstChar);
-int isLabel(char * label);
-FirstScan * doScan(char * asFile, int strLen);
+int checkEmpty(char * line); /*gets a line, checks if it's empty*/
+int isAlphabet(char firstChar); /*gets a char and return if it's alphbet char*/
+int isLabel(char * label); /* gets a line and checks whether there is a label in it*/
+int isSavedWord(char * label); /* gets a label and cheks if it's a saved word*/
+FirstScan * doScan(char * asFile, int strLen); /*scan it self*/
 
 #endif
