6c711a75773252e66cdc79127d06cc040f22aa72
adishik <60340634+adishik@users.noreply.github.com>
2021-08-13 03:07:29 +0300

    Update secondScan.h

--- secondScan.h
+++ secondScan.h
@@ -3,7 +3,7 @@
 #ifndef __SECOND_SCAN__
 #define __SECOND_SCAN__
 
-
+/* second scan, gets the result of the first*/
 FirstScan * doSecondScan(FirstScan  * firstScan, char * asFile, int strLen);
 
 #endif
