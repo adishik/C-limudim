6295973ab56b53648f4257e087aefdb7d04d8d12
adishik <60340634+adishik@users.noreply.github.com>
2021-08-13 03:04:42 +0300

    Update actionTable.h

--- actionTable.h
+++ actionTable.h
@@ -5,6 +5,7 @@
 
 #ifndef __ACTION_TABLE__
 #define __ACTION_TABLE__
+/* this moudle is for hanlding actions, I used an array of structers in order to keep all of the action attribuites*/
 
 struct _Action
 {
@@ -16,10 +17,10 @@ struct _Action
 };
 typedef struct _Action Action;
 
-int * convDectoBin(int decimalNumber, int  * binaryNumber); 
-Action  * getAction(char * actionName); 
+int * convDectoBin(int decimalNumber, int  * binaryNumber); /*convert Decimal number to binary*/
+Action  * getAction(char * actionName); /* Return action pointer according ot the action name*/
 
-int * codeAction(Action  * action, int rs, int rt, int rd, int imme, int reg, int add ); 
-
+int * codeAction(Action  * action, int rs, int rt, int rd, int imme, int reg, int add ); /*coding the action according tot he specification*/
+int checkActionSyntax(Action * tempAction, char * line, int strLine); /*checks action validity*/
 #endif
 
