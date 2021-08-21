592e1f7167db66257fe6968c9988a7a64649f891
adishik <60340634+adishik@users.noreply.github.com>
2021-08-21 12:53:44 +0300

    Update instructions.c

--- instructions.c
+++ instructions.c
@@ -4,7 +4,7 @@
 #define byte 1
 #define true 1
 #define false 0
-#define MAX_LABLE_LEN 60
+#define MAX_LABLE_LEN 80
 
 
 int  ** freeDInstructions(char instructionType,int * intstructionParms, int parmsSize) /* clear spce for .d instructions*/
@@ -18,7 +18,7 @@ int  ** freeDInstructions(char instructionType,int * intstructionParms, int parm
         {
             instructionpPointer[k] = (int*)malloc(32 * sizeof(int));
             tempBin = (int*)malloc(32 * sizeof(int));
-            tempBin = convDectoBin(intstructionParms[k],tempBin);
+            tempBin = convDectoBin(intstructionParms[k],tempBin, 32);
 
             w = 31;
             for (i = 0; i < 32; i++)
@@ -39,9 +39,9 @@ int ** freeAsInstruction(char * parm) /* clear spce for .asciz instructions*/
     int i = 0, k = 0;
     for(k = 0; k < strlen(parm); k++)
         {
-            instructionpPointer[k] = (int*)malloc(sizeof(byte));
-            tempBin = (int*)malloc(sizeof(byte));
-            tempBin = convDectoBin((int)parm[k],tempBin);
+            instructionpPointer[k] = (int*)malloc(sizeof(int));
+            tempBin = (int*)malloc(8 * sizeof(int));
+            tempBin = convDectoBin((int)parm[k],tempBin, 8);
             for (i = 0; i < 8; i++)
             {
                 instructionpPointer[k][i] = tempBin[i];
@@ -55,7 +55,7 @@ int ** freeAsInstruction(char * parm) /* clear spce for .asciz instructions*/
 
 }
 
-int checkInstructionSyntax(char * node, int strLen)
+int checkInstructionSyntax(char * node, int strLen) /* checks fo syntax errors */
 {
     char  * temp;
     int i = 0, z = 0;
@@ -64,7 +64,7 @@ int checkInstructionSyntax(char * node, int strLen)
     temp = (char*)malloc(strLen * sizeof(char));        
     for (i = 0; i < strLen ; i++)
     {
-        if(node[i] != ' ')
+        if(node[i] != ' ') /* Clear Spaces */
         {
             temp[i] = node[i];
         }
@@ -74,13 +74,13 @@ int checkInstructionSyntax(char * node, int strLen)
 
     for (i = 0; i < z ; i++)
     {
-        if(temp[i] == temp[i + 1] && (temp[i] == ','))
+        if(temp[i] == temp[i + 1] && (temp[i] == ',')) /* Checking if thee is ,, on the line */
         {
             return 1;
         }
     }
 
-    if(strLen > 60)
+    if(strLen > 80) /* check if the line's length is valid */
     {
         return 2;
     }
